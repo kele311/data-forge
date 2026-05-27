c814f236e0bbd52b60cffb4b98386ae524eb3d0c
kele311 <jakovgradiska@gmail.com>
2026-05-27 18:15:44 +0200

    Main library export

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * data-forge - Lightning-fast CSV ↔ JSON converter and validator
+ */
+
+export {
+  csvToJson,
+  jsonToCsv,
+  convertFile,
+  validateCsv,
+  validateJson,
+  ConvertOptions,
+  ValidationResult,
+} from './converter';
